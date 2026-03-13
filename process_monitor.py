3ef7859837f797f04a9bc091a93278e832407866
SedELB <saadoun500@gmail.com>
2026-03-13 16:11:52 -0400

    Initial commit of existing files

---
+++ process_monitor.py
@@ -0,0 +1,14 @@
+import psutil as ps
+
+def is_process_running(exe_name: str) -> bool:
+    for process in ps.process_iter(attrs=['name']):
+        if process.info['name'] == exe_name:
+            print('Process found !')
+            return True
+
+    print('Process not found')
+    return False
+
+    
+
+
