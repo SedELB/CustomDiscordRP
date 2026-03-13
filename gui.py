1eac7532b52b897cd2e98fb319471ba143421712
SedELB <saadoun500@gmail.com>
2026-03-13 16:45:00 -0400

    feat(gui.py): Basic GUI with start/stop buttons and inputs for Presence information

---
+++ gui.py
@@ -0,0 +1,38 @@
+import customtkinter
+
+def button_callback():
+    print("button clicked")
+
+def get_data():
+    data = {}
+
+    data['clientId'] = clientIdEntry.get()
+    data['targetExe'] = targetExeEntry.get()
+    data['statusName'] = statusNameEntry.get()
+    data['statusState'] = statusStateEntry.get()
+
+    print(data)
+    return data
+
+app = customtkinter.CTk()
+app.geometry("800x600")
+
+startButton = customtkinter.CTkButton(app, text="Start", command=get_data)
+stopButton = customtkinter.CTkButton(app, text="Stop", command=button_callback)
+
+clientIdEntry = customtkinter.CTkEntry(app, placeholder_text='Client ID')
+targetExeEntry = customtkinter.CTkEntry(app, placeholder_text='Target .exe')
+
+statusNameEntry = customtkinter.CTkEntry(app, placeholder_text='Your custom status name...')
+statusStateEntry = customtkinter.CTkEntry(app, placeholder_text='Your custom state...')
+
+startButton.pack()
+stopButton.pack()
+
+clientIdEntry.pack()
+targetExeEntry.pack()
+
+statusNameEntry.pack()
+statusStateEntry.pack()
+
+app.mainloop()
